8995cfc006ec4019b87cd8403c4ca0ed816b213d
jtong <tj19832@gmail.com>
2013-10-27 16:52:37 +0800

    jtong - take charge of your own responsibility

--- web/js/controller/sign_up_controller.js
+++ web/js/controller/sign_up_controller.js
@@ -1,33 +1,21 @@
 function SignUpController($scope, $navigate) {
 
     (function init() {
-        if (localStorage.getItem("activity_status") == "before_running" || localStorage.getItem("activity_status") == null) {
-            $scope.start_or_end_label = "开始";
-        }
-        if (localStorage.getItem("activity_status") == "running") {
-            $scope.start_or_end_label = "结束";
-        }
-        if (localStorage.getItem("activity_status") == "ran") {
-            $scope.start_or_end_label = "结束";
-        }
+
     })();
 
-    $scope.record_sign_up_status = function () {
+    $scope.selection = localStorage.getItem("activity_status");
 
-        if (localStorage.getItem("activity_status") == "running") {
-            if (window.confirm("确实要结束活动报名吗？")) {
-                localStorage.setItem("activity_status", "ran");
-                $scope.start_or_end_label = "结束";
-                //go_to_bid_list_page();
-            }
-        }
-        if (localStorage.getItem("activity_status") == "before_running" || localStorage.getItem("activity_status") == null) {
-                localStorage.setItem("activity_status", "running");
-                $scope.start_or_end_label = "结束";
-        }
-        if(localStorage.getItem("activity_status") == "ran"){
-            localStorage.setItem("activity_status", "before_running");
-            $scope.start_or_end_label = "开始";
+    $scope.start = function () {
+        localStorage.setItem("activity_status", "running");
+        $scope.selection = localStorage.getItem("activity_status");
+    }
+
+    $scope.end = function () {
+        if(window.confirm("确实要结束活动报名吗？")){
+            localStorage.setItem("activity_status","ran");
+            $scope.selection = localStorage.getItem("activity_status");
+
         }
     }
 }
